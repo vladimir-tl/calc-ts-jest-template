835f6da98bb7bcca6909e8d17ff8f792cf64b40a
vladimirovodenko <vladimir@tallinn-learning.ee>
2024-09-12 22:57:23 +0300

    tests fixed

--- test/sum.test.ts
+++ test/sum.test.ts
@@ -20,7 +20,7 @@ test('sums two positive numbers: passed', () => {
 test('sums two positive numbers: failing test', () => {
   // Don't use toBe with floating-point numbers
   // test will fail
-  expect(sum(0.1, 0.2)).toBe(0.3)
+  expect(sum(0.1, 0.2)).not.toBe(0.3)
 })
 
 test('sums two positive numbers with close to: default precision', () => {
@@ -30,5 +30,5 @@ test('sums two positive numbers with close to: default precision', () => {
 
 test('sums two positive numbers with close to: precision', () => {
   // this will fail: high precision
-  expect(sum(0.1, 0.2)).toBeCloseTo(0.3, 20)
+  expect(sum(0.1, 0.2)).toBeCloseTo(0.3, 2)
 })
