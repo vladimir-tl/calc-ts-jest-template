974acbf2fa1f9f6fd69083cf955b98399d28e75d
vladimirovodenko <vladimir@tallinn-learning.ee>
2024-09-13 08:41:44 +0300

    intentionally broken code

--- src/math-operations.ts
+++ src/math-operations.ts
@@ -1,9 +1,5 @@
 export class MathOperations {
   add(a: number, b: number): number {
-    return a + b
-  }
-
-  subtract(a: number, b: number): number {
-    return a - b
+    return a + b + 1
   }
 }
